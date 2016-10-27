e06f36228b55918f8dc4479fcb1837bb3820195f
王彬彬 <wangbinbin@wangbinbins-MacBook-Pro.local>
2016-10-27 18:18:40 +0800

    move gotcp

---
+++ src/github.com/gansidui/gotcp/examples/echo/client_echo/client_echo.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net"
+	"time"
+	"strconv"
+
+	"github.com/gansidui/gotcp/examples/echo"
+)
+
+func main() {
+	tcpAddr, err := net.ResolveTCPAddr("tcp4", "127.0.0.1:8989")
+	checkError(err)
+	conn, err := net.DialTCP("tcp", nil, tcpAddr)
+	checkError(err)
+
+	echoProtocol := &echo.EchoProtocol{}
+
+	now := time.Now().UnixNano()
+	// ping <--> pong
+	for i := 0; i < 30000; i++ {
+		// write
+		conn.Write(echo.NewEchoPacket([]byte("hello - "+strconv.Itoa(i)), false).Serialize())
+
+		// read
+		p, err := echoProtocol.ReadPacket(conn)
+		if err == nil {
+			echoPacket := p.(*echo.EchoPacket)
+			fmt.Printf("Server reply:[%v] [%v]\n", echoPacket.GetLength(), string(echoPacket.GetBody()))
+		}
+
+//		time.Sleep(2 * time.Second)
+	}
+    now2 := time.Now().UnixNano()
+	fmt.Printf("cost time:%dms\n", (now2-now)/1000000)
+	conn.Close()
+}
+
+func checkError(err error) {
+	if err != nil {
+		log.Fatal(err)
+	}
+}
